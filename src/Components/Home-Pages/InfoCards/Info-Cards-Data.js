9440d1c2eb9719f3302406c11d7600d332b34add
Naveen-byte-gif <tungananaveenjob18@gmail.com>
2025-06-16 21:27:53 +0530

    Home page design

---
+++ src/Components/Home-Pages/InfoCards/Info-Cards-Data.js
@@ -0,0 +1,24 @@
+import { FaThLarge, FaCalendarAlt, FaCar, FaIdCard } from 'react-icons/fa';
+
+export const cards = [
+  {
+    icon: <FaThLarge />,
+    title: 'Driving Schools',
+    subtitle: 'Find a top-rated schools near you.',
+  },
+  {
+    icon: <FaCalendarAlt />,
+    title: 'Easy Bookings',
+    subtitle: 'Schedule lessons instantly.',
+  },
+  {
+    icon: <FaCar />,
+    title: 'Learn About Cars',
+    subtitle: 'Explore different car models.',
+  },
+  {
+    icon: <FaIdCard />,
+    title: 'License Guide',
+    subtitle: 'Step-by-Step Application process.',
+  },
+];
--- src/Components/Nav-Bar/Nav.js
+++ src/Components/Nav-Bar/Nav.js
@@ -1,32 +1,49 @@
-import React from 'react';
-import './NavBar.css'; 
-import logo from '../../svg-Images/logo.svg';
+import React from "react";
+import "./NavBar.css";
+import logo from "../../svg-Images/logo.svg";
+import { Link } from "react-router-dom";
 
 const NavBarHead = () => {
   return (
     <nav className="navbar navbar-expand-lg custom-navbar fixed-top shadow-sm">
       <div className="container-fluid">
-        <a className="navbar-brand d-flex align-items-center text-white" href="#">
-          <img src={logo}/>
+        <a
+          className="navbar-brand d-flex align-items-center text-white"
+          href="#"
+        >
+          <img src={logo} />
         </a>
 
-        <button className="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <button
+          className="navbar-toggler bg-white"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav mx-auto">
             <li className="nav-item">
-              <a className="nav-link text-white" href="#">Home</a>
+              <a className="nav-link text-white" href="#">
+                Home
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-white" href="#">About Us</a>
+              <a className="nav-link text-white" href="#">
+                About Us
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-white" href="#">Services</a>
+              <a className="nav-link text-white" href="#">
+                Services
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-white" href="#">Contact Us</a>
+              <a className="nav-link text-white" href="#">
+                Contact Us
+              </a>
             </li>
           </ul>
 
@@ -38,11 +55,14 @@ const NavBarHead = () => {
               <option>Delhi</option>
             </select>
 
-          
-
-            <button className="btn btn-white login-button">
-              Login   <span className="text-primary">Register</span>
-            </button>
+            <div className="d-flex gap-2">
+              <Link to="/login" className="btn btn-white login-button">
+                Login
+              </Link>
+              <Link to="/register" className="btn btn-white login-button">
+                Register
+              </Link>
+            </div>
           </div>
         </div>
       </div>
